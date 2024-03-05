4cbb165982e8e22f180e0ab2cfa2e7f7b925c933
jameswu <jamesw0462@gmail.com>
2024-03-05 14:13:08 -0800

    Updated table to reflect correct data, updated dataset overview and examples

--- static/js/leaderboard_testmini.js
+++ static/js/leaderboard_testmini.js
@@ -1,106 +1,50 @@
 function generateTable() {
-  var data = score_table; // The variable from model_scores.js
+	var data = score_table; // The variable from model_scores.js
 
-  var table = '<table class="js-sort-table" id="results">';
-  table += `<tr>
-          <td class="js-sort-number"><strong>#</strong></td>
-          <td class="js-sort"><strong>Model</strong></td>
-          <td class="js-sort"><strong>Method</strong></td>
-          <td class="js-sort"><strong>Source</strong></td>
-          <td class="js-sort"><strong>Date</strong></td>
-          <td class="js-sort-number"><strong><u>ALL</u></strong></td>
-          <td class="js-sort-number"><strong>FQA</strong></td>
-          <td class="js-sort-number"><strong>GPS</strong></td>
-          <td class="js-sort-number"><strong>MWP</strong></td>
-          <td class="js-sort-number"><strong>TQA</strong></td>
-          <td class="js-sort-number"><strong>VQA</strong></td>
-          <td class="js-sort-number"><strong>ALG</strong></td>
-          <td class="js-sort-number"><strong>ARI</strong></td>
-          <td class="js-sort-number"><strong>GEO</strong></td>
-          <td class="js-sort-number"><strong>LOG</strong></td>
-          <td class="js-sort-number"><strong>NUM</strong></td>
-          <td class="js-sort-number"><strong>SCI</strong></td>
-          <td class="js-sort-number"><strong>STA</strong></td>
-      </tr>`;
+	var table = '<table class="js-sort-table" id="results">';
+	table += `<tr>
+        <td class="js-sort"><strong>Model</strong></td>
+        <td class="js-sort"><strong>atkins</strong></td>
+        <td class="js-sort"><strong>chemmc</strong></td>
+        <td class="js-sort"><strong>quan</strong></td>
+        <td class="js-sort-number"><strong>matter</strong></td>
+        <td class="js-sort-number"><strong>fund</strong></td>
+        <td class="js-sort-number"><strong>class</strong></td>
+        <td class="js-sort-number"><strong>thermo</strong></td>
+        <td class="js-sort-number"><strong>diff</strong></td>
+        <td class="js-sort-number"><strong>stat</strong></td>
+        <td class="js-sort-number"><strong>calc</strong></td>
+        <td class="js-sort-number"><strong>Avg</strong></td>
+    </tr>`;
 
-      // sort data to make sure the best model is on top
-      first_row = '-' // "Human Performance*"
-      console.log(data);
+	// Iterate through the outer sections
+	for (var section in data) {
+		// Add section title row
+		table += `<tr class="section-title"><td colspan="13">${section}</td></tr>`;
 
-      // get all keys in data
-      var keys = Object.keys(data);
+		// Iterate through the inner models within each section
+		for (var modelName in data[section]) {
+			var entry = data[section][modelName];
 
-      // remove "Human Performance*" from keys
-      var index = keys.indexOf(first_row);
-      if (index > -1) {
-        keys.splice(index, 1);
-      }
+			table += '<tr>';
+			table += `<td><b>${modelName}</b></td>`;
+			table += `<td>${entry.atkins}</td>`;
+			table += `<td>${entry.chemmc}</td>`;
+			table += `<td>${entry.quan}</td>`;
+			table += `<td>${entry.matter}</td>`;
+			table += `<td>${entry.fund}</td>`;
+			table += `<td>${entry.class}</td>`;
+			table += `<td>${entry.thermo}</td>`;
+			table += `<td>${entry.diff}</td>`;
+			table += `<td>${entry.stat}</td>`;
+			table += `<td>${entry.calc}</td>`;
+			table += `<td>${entry.Avg}</td>`;
+			table += '</tr>';
+		}
+	}
 
-      // add "Human Performance*" to the top of keys
-      keys.unshift(first_row);
-
-      console.log(keys);
-
-      // for (var key in data) {
-      for (var i=0; i<keys.length; i++) {
-        var key = keys[i];
-        console.log(key);
-        var entry = data[key];
-
-        table += '<tr>';
-        table += `<td>${key}</td>`
-
-        // for key = "1", "2", "3"
-        top_ranks = ["1", "2", "3"]
-        if (top_ranks.includes(key)) {
-          table += `<td><b class="best-score-text">${entry.Model}</b></td>`;
-          table += `<td>${entry.Method}</td>`;
-          table += `<td><a href="${entry.Source}" class="ext-link" style="font-size: 16px;">Link</a></td>`;
-          table += `<td>${entry.Date}</td>`;
-          table += `<td><b class="best-score-text">${entry.ALL.toFixed(1).toString()}</b></td>`; // .toFixed(1): round to 1 decimal place
-        }
-        else {
-          table += `<td><b>${entry.Model}</b></td>`;
-          table += `<td>${entry.Method}</td>`;
-          table += `<td><a href="${entry.Source}" class="ext-link" style="font-size: 16px;">Link</a></td>`;
-          table += `<td>${entry.Date}</td>`;
-          table += `<td><b>${entry.ALL.toFixed(1).toString()}</b></td>`; // .toFixed(1): round to 1 decimal place
-        }          
-
-        // if entry.FQA is a number
-        if (!isNaN(entry.FQA)) {
-          table += `<td>${entry.FQA.toFixed(1).toString()}</td>`;
-          table += `<td>${entry.GPS.toFixed(1).toString()}</td>`;
-          table += `<td>${entry.MWP.toFixed(1).toString()}</td>`;
-          table += `<td>${entry.TQA.toFixed(1).toString()}</td>`;
-          table += `<td>${entry.VQA.toFixed(1).toString()}</td>`;
-          table += `<td>${entry.ALG.toFixed(1).toString()}</td>`;
-          table += `<td>${entry.ARI.toFixed(1).toString()}</td>`;
-          table += `<td>${entry.GEO.toFixed(1).toString()}</td>`;
-          table += `<td>${entry.LOG.toFixed(1).toString()}</td>`;
-          table += `<td>${entry.NUM.toFixed(1).toString()}</td>`;
-          table += `<td>${entry.SCI.toFixed(1).toString()}</td>`;
-          table += `<td>${entry.STA.toFixed(1).toString()}</td>`;
-        }
-        else {
-        table += `<td>${entry.FQA.toString()}</td>`;
-        table += `<td>${entry.GPS.toString()}</td>`;
-        table += `<td>${entry.MWP.toString()}</td>`;
-        table += `<td>${entry.TQA.toString()}</td>`;
-        table += `<td>${entry.VQA.toString()}</td>`;
-        table += `<td>${entry.ALG.toString()}</td>`;
-        table += `<td>${entry.ARI.toString()}</td>`;
-        table += `<td>${entry.GEO.toString()}</td>`;
-        table += `<td>${entry.LOG.toString()}</td>`;
-        table += `<td>${entry.NUM.toString()}</td>`;
-        table += `<td>${entry.SCI.toString()}</td>`;
-        table += `<td>${entry.STA.toString()}</td>`;
-        }
-        table += '</tr>';
-    }
-
-  table += '</table>';
-  document.getElementById('testmini_leaderboard').innerHTML = table; // Assuming you have a div with id 'container' where the table will be placed
+	table += '</table>';
+	document.getElementById('textbook_leaderboard').innerHTML = table;
 }
 
 // Call the function when the window loads
